508e7d74fdfb2a7361a60b777e11621e7549ddb4
Eugene Krevenets <hyzhak@ya.ru>
2015-11-26 23:36:03 +0100

    add proxyUrl option

--- test/utils.test.js
+++ test/utils.test.js
@@ -13,11 +13,11 @@ describe('utils', function() {
   });
 
   it('should reject Promise if there no any items are passed', function(done) {
-    nock('http://some.dom')
+    nock('http://some.org')
       .get('/url')
       .reply(200, 'Hello World!');
 
-    batchRequestAsync('http://some.dom/url')
+    batchRequestAsync('http://some.org/url')
       .then(function() {
         done('should reject Promise');
       })
@@ -26,12 +26,12 @@ describe('utils', function() {
       });
   });
 
-  it('should do single request GET http://some.dom/url/:id and return result as Promise on successful response', function(done) {
-    nock('http://some.dom')
+  it('should do single request GET http://some.org/url/:id and return result as Promise on successful response', function(done) {
+    nock('http://some.org')
       .get('/url/123456')
       .reply(200, 'Hello World!');
 
-    batchRequestAsync('http://some.dom/url/', 123456)
+    batchRequestAsync('http://some.org/url/', 123456)
       .then(function(res) {
         expect(res[0][0]).to.equal('Hello World!');
         done();
@@ -39,16 +39,16 @@ describe('utils', function() {
       .catch(done);
   });
 
-  it('should do multi request GET http://some.dom/url/:id and return result as Promise on successful response', function(done) {
-    nock('http://some.dom')
+  it('should do multi request GET http://some.org/url/:id and return result as Promise on successful response', function(done) {
+    nock('http://some.org')
       .get('/url/1')
       .reply(200, 'Hello World 1!');
 
-    nock('http://some.dom')
+    nock('http://some.org')
       .get('/url/2')
       .reply(200, 'Hello World 2!');
 
-    batchRequestAsync('http://some.dom/url/', [1, 2])
+    batchRequestAsync('http://some.org/url/', [1, 2])
       .then(function(res) {
         expect(res[0][0]).to.equal('Hello World 1!');
         expect(res[1][0]).to.equal('Hello World 2!');
@@ -57,19 +57,31 @@ describe('utils', function() {
       .catch(done);
   });
 
-  it('should do multi request GET http://some.dom/url/:id and http://another.dom/url/:id and return result as Promise on successful response', function(done) {
-    nock('http://one.dom')
+  it('should do multi request GET http://some.org/url/:id and http://another.org/url/:id and return result as Promise on successful response', function(done) {
+    nock('http://one.org')
       .get('/url/1')
       .reply(200, 'Hello One World!');
 
-    nock('http://another.dom')
+    nock('http://another.org')
       .get('/url/1')
       .reply(200, 'Hello Another World!');
 
-    batchRequestAsync(['http://one.dom/url/', 'http://another.dom/url/'], 1)
+    batchRequestAsync(['http://one.org/url/', 'http://another.org/url/'], 1)
       .then(function(res) {
         expect(res[0][0]).to.equal('Hello One World!');
         expect(res[0][1]).to.equal('Hello Another World!');
+        done();
+      })
+      .catch(done);
+  });
+
+  it('should request thought proxy if it passed to options', function(done) {
+    nock('http://proxy.org')
+      .get('/url?query=' + encodeURIComponent('http://one.org/url/1'))
+      .reply(200);
+
+    batchRequestAsync('http://one.org/url/', 1, {proxyUrl: 'http://proxy.org/url?query='})
+      .then(function() {
         done();
       })
       .catch(done);
