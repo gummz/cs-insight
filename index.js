7d87513828ecb4e2468b21f59fdd73379f9b8f9c
Eugene Krevenets <hyzhak@ya.ru>
2015-12-02 20:06:35 +0100

    use proxy for each GET, POST request if it was passed to the API wrapper

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var Addresses = require('./lib/addresses');
 var Blocks = require('./lib/blocks');
 var LightTransactions = require('./lib/transactions/light');
 var Transactions = require('./lib/transactions');
-var utils = require('./lib/utils');
 
 var NETWORKS = {
   testnet: 'test-insight',
@@ -18,14 +17,12 @@ function Wrapper(network, proxyURL) {
   var BASE_URL = 'https://' + NETWORKS[network] + '.bitpay.com/api/';
 
   // end points
-  this.lightTransactions = new LightTransactions(BASE_URL);
-  this.blocks = new Blocks(BASE_URL, this.lightTransactions);
-  this.transactions = new Transactions(BASE_URL, this.blocks);
-  this.addresses = new Addresses(BASE_URL, this.blocks, this.lightTransactions);
+  this.lightTransactions = new LightTransactions(BASE_URL, proxyURL);
+  this.blocks = new Blocks(BASE_URL, proxyURL, this.lightTransactions);
+  this.transactions = new Transactions(BASE_URL, proxyURL, this.blocks);
+  this.addresses = new Addresses(BASE_URL, proxyURL, this.blocks, this.lightTransactions);
 
   this.network = network;
-
-  utils.setProxyURL(proxyURL);
   this.proxyURL = proxyURL;
 }
 
