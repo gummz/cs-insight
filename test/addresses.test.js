930858faaea9d3aac3077d4bb6ce34b354a121ae
Eugene Krevenets <hyzhak@ya.ru>
2015-12-20 23:29:49 +0100

    be able to request transactions for a group of addresses

--- test/addresses.test.js
+++ test/addresses.test.js
@@ -89,6 +89,27 @@ describe('Addresses', function() {
           done();
         });
     });
+
+    it('should be able to request group of addresses', function(done) {
+      addresses
+        .transactions([
+          '2NBXcxopF9gs8sg9KfgPtKRsVZ6HyWVPHQY',
+          'mpNDUWcDcZw1Teo3LFHvr8usNdwDLKdTaY'
+        ])
+        .then(function(res) {
+          console.log('res', res);
+          expect(res).to.be.instanceof(Array);
+          expect(res).has.length(3);
+          expect(res[0]).to.has.property('blockId', '00000000000007dc622b16ab6698ad580c69e7e21c59bc9474418e85c6f3489a');
+          expect(res[0]).to.has.property('txId', '6560380b5b8fc147e760545ff72eb3aca05116326509dea0157faf94dbe4ac32');
+          expect(res[1]).to.has.property('blockId', '00000000000010efb93b48b18d489d9e959997dd4f9e2acaf3191ad9ec1aa3e4');
+          expect(res[1]).to.has.property('txId', '4979a0b69703f888dc5936a4be039dabb976fae7d45604d57b5fad35b3c94200');
+          expect(res[2]).to.has.property('blockId', '00000000a1e890e1c2cfe6edf939b83b9a4d6fd4b066324b84f67660215887b0');
+          expect(res[2]).to.has.property('txId', 'ffd316b0c4feb9d29c61c3734fcde0167600441e560931c8c7267a9de3d9e29a');
+          done();
+        })
+        .catch(done);
+    });
   });
 
   describe('unspents', function() {
